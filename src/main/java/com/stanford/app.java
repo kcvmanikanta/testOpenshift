46edc404adc93f0d8324e5da6d81adfafe4bdfaa
MANI <kchaitanya.nani@gmail.com>
2018-09-17 17:24:50 -0700

    testOpenshift

---
+++ src/main/java/com/stanford/app.java
@@ -0,0 +1,12 @@
+package com.stanford;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLPServer;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class app {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
